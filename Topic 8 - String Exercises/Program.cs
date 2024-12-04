f19020aebf5e4df43f2b9635e6d1ef5bb1afc986
Jack Gallagher <339690067@GoTVDSB.ca>
2024-12-04 09:05:45 -0500

    Add project files.

---
+++ Topic 8 - String Exercises/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_8___String_Exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
